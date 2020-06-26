21c70e4052e2b0422683ab5df1e9a027b96b5ff0
attilakaradeniz <attilakaradeniz@gmail.com>
2020-06-26 02:14:18 +0200

    first commit working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { StorageService, Item } from '../services/storage.service';
+import { Platform, ToastController } from '@ionic/angular';
+//import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
-  constructor() {}
+  items: Item[] = [];
+
+  newItem: Item = <Item>{};
+
+ // @ViewChild('myList')myList: IonList;
+
+  constructor(private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+
+  // create
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+
+    this.storageService.addItem(this.newItem).then(item => {
+      this.newItem = <Item>{};
+      this.showToast('Yummed joyfully');
+      this.loadItems();
+      console.log('newItem: ', this.newItem);
+      console.log('items', this.items);
+
+    });
+  }
+
+    // read
+    loadItems() {
+      this.storageService.getItems().then(items => {
+        this.items = items;
+      });
+    }
+
+
+
+
+  // update
+  updateItem(item: Item) {
+    item.title = `YUMMM: ${item.title}`;
+    item.modified = Date.now();
+
+    this.storageService.updateItem(item).then(item => {
+      this.showToast('YUMMMPDATED!');
+      // this.myList.closeSlidingItems();
+      this.loadItems();
+    });
+  }
+
+  // delete
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then(item => {
+      this.showToast('wish I ate that :(');
+      // this.myList.closeSlidingItems();
+      this.loadItems();
+    });
+  }
+
+  // helper
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      // cssClass: 'toast-bg',
+      buttons: [
+       {
+         icon:'star',
+         
+       }
+      ],
+      color: 'dark'
+      
+    });
+    toast.present();
+  }
+
 
 }
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+export interface Item {
+  id: number,
+  title: string,
+  ingredients: string,
+  instructions: string,
+  modified: number,
+  nutFacts: string
+  }
+
+  const ITEMS_KEY = 'food-items';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor(private storage: Storage) { }
+
+  // create
+  addItem(item: Item): Promise<any> {
+    return this.storage.get(ITEMS_KEY).then((items: Item[]) => {
+      if(items) {
+        items.push(item); 
+        return this.storage.set(ITEMS_KEY, items);
+      }else {
+        return this.storage.set(ITEMS_KEY, [item]);
+      }
+    });
+
+  }
+
+  // read
+  getItems(): Promise<Item[]> {
+      return this.storage.get(ITEMS_KEY);
+  }
+
+
+  
+  // update
+  updateItem(item: Item): Promise<any> {
+    return this.storage.get(ITEMS_KEY).then((items: Item[]) => {
+      if(!items || items.length === 0) {
+        return null;
+      }
+
+      let newItems: Item[] = [];
+      
+      for(let i of items) {
+        if(i.id === item.id) {
+          newItems.push(item); 
+        } else {
+          newItems.push(i);
+        }
+      }
+      return this.storage.set(ITEMS_KEY, newItems);
+    });
+  }
+
+
+  // delete
+  deleteItem(id: number): Promise<Item> {
+      return this.storage.get(ITEMS_KEY).then((items: Item[]) => {
+          if(!items || items.length === 0) {
+            return null;
+          }
+          
+          let toKeep: Item[] = [];
+
+          for(let i of items) {
+            if(i.id !== id) {
+              toKeep.push(i);
+            }
+          }
+
+          return this.storage.set(ITEMS_KEY, toKeep);
+      });
+  }
+
+
+
+
+}
